467f06984207c7e795d3a85d06752685279f45b2
Zakiego <belummikir@gmail.com>
2022-02-15 11:18:08 +0800

    feat: add helper function

---
+++ components/Helper.ts
@@ -0,0 +1,11 @@
+export function ShuffleArray(array: string[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export function SplitString(list: string): string[] {
+  return list.split(/\r?\n/).filter(String);
+}
