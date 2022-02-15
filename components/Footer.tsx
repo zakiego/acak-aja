eaeef7151504feaf3dbd497630eb9acc2da92b34
Zakiego <belummikir@gmail.com>
2022-02-15 11:18:16 +0800

    feat: add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="mb-10 text-sm opacity-70">
+      Made with 🤍 by{" "}
+      <a href="https://zakiego.my.id" className="link">
+        Zakiego
+      </a>
+    </div>
+  );
+}
