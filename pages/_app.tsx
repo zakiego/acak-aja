24666f6088069444894b3e7d8d74d516f816a856
Zakiego <belummikir@gmail.com>
2022-02-17 18:42:54 +0800

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,24 @@ import "~/styles/globals.css";
 
 // import App from "next/app";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
+
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider defaultTheme={"light"} attribute="class">
       <Component {...pageProps} />
