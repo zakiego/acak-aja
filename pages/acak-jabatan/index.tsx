2cb3be7c0be7c401690c39498196de3444c80b32
Zakiego <belummikir@gmail.com>
2022-02-20 19:22:28 +0800

    feat: add amount of each jabatan

--- pages/acak-jabatan/index.tsx
+++ pages/acak-jabatan/index.tsx
@@ -3,19 +3,30 @@ import { useState } from "react";
 
 import InputForm from "~/components/acak-jabatan/InputForm";
 import RandomResult from "~/components/acak-jabatan/RandomResult";
-import { ShuffleArray } from "~/components/Helper";
+import {
+  FilterArrayJSONNotNull,
+  GroupByNGroupJabatan,
+  Position,
+  ShuffleArray,
+} from "~/components/Helper";
 import Layout from "~/components/Layout";
 
 export default function AcakJabatan() {
-  const [listName, setListName] = useState<string[]>([]);
-  const [listPosition, setListPosition] = useState<string[]>([""]);
+  const [listPosition, setListPosition] = useState<Position[]>([
+    { name: "", amount: 1, member: [] },
+  ]);
+  const [groups, setGroups] = useState<Position[]>([]);
 
-  async function randomFunction(listName: string[]) {
-    setListName(ShuffleArray([...listName]) as string[]);
+  async function randomFunction(tempListName: string[]) {
+    const data = GroupByNGroupJabatan(
+      FilterArrayJSONNotNull(listPosition),
+      ShuffleArray([...tempListName]) as string[],
+    );
+    setGroups(data);
   }
 
   const handleAddClick = () => {
-    setListPosition([...listPosition, ""]);
+    setListPosition([...listPosition, { name: "", amount: 1, member: [] }]);
   };
 
   const handleRemoveClick = (index: number) => {
@@ -24,13 +35,19 @@ export default function AcakJabatan() {
     setListPosition(list);
   };
 
-  const handleInputChange = (
+  const handleInpuChange = (
     e: React.ChangeEvent<HTMLInputElement>,
     index: number,
+    propertyToChange: "name" | "amount",
   ) => {
     const { value } = e.target;
     const list = [...listPosition];
-    list[index] = value;
+    if (propertyToChange == "amount") {
+      list[index][propertyToChange] = parseInt(value);
+    }
+    if (propertyToChange == "name") {
+      list[index][propertyToChange] = value;
+    }
     setListPosition(list);
   };
 
@@ -44,19 +61,18 @@ export default function AcakJabatan() {
 
       <div className=" space-y-6 divide-y-2 divide-dashed">
         <InputForm
-          handleInputChange={handleInputChange}
+          setListPosition={setListPosition}
           handleRemoveClick={handleRemoveClick}
+          handleInpuChange={handleInpuChange}
           handleAddClick={handleAddClick}
           randomFunction={randomFunction}
-          setListName={setListName}
-          setListPosition={setListPosition}
           listPosition={listPosition}
         />
 
-        {listName.length > 0 && (
+        {groups.length > 0 && (
           <div>
             <div className="mt-6">
-              <RandomResult listName={listName} listPosition={listPosition} />
+              <RandomResult groups={groups} />
             </div>
           </div>
         )}
